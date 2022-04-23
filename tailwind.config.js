94a3f03059a701e6c2e9e28bf703467d60148df5
yugo-ibuki <y.ibuki91@gmail.com>
2022-04-24 01:25:56 +0900

    fix: tailwind configに追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,19 @@
 module.exports = {
-  content: ['./src/pages/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
-  theme: { extend: {} },
+  content: ['./src/pages/**/*.{ts,tsx}', './src/**/*.{ts,tsx}', './src/style/global.css'],
+  darkMode: false,
+  theme: {
+    screens: {
+      sp: { max: '768px' },
+      // --------------------------------------------------
+      // tailwind default
+      // --------------------------------------------------
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px'
+    },
+    extend: {}
+  },
+  variants: {},
   plugins: [],
 }
